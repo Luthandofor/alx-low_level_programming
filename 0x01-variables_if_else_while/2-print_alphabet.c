fab166b2db7e8a307f24b8538c660b378c171eeb
Luthandofor <Noluthando.dyantyi1@gmail.com>
2023-02-17 10:44:52 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - returns alphabets
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+int ch = 'a';
+while (ch <= 'z');
+{
+putchar(ch);
+ch++
+}
+putchar('\n');
+return (0);
+}
